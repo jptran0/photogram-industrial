467bd6d9131c5be4d89107b759de6a24df1fd17a
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-11-09 20:14:23 +0000

    set up rcav for user profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
+  #get "/users/:id" => "users#show", as: :user
+  #resources :users, only :show
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "photos#index"
   # root "articles#index"
+
+  get "/:username" => "users#show", as: :user
+
 end
