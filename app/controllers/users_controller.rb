f1489b404cd06f651db58b5ccc2974608fd26998
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-11-09 20:19:33 +0000

    Set up users#show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    #the exclamation point returns a "record not found" error which would show a 404 error instead of an internal 500-type server error which is what we want
+    @user = User.find_by!(username: params.fetch(:username))
   end
 end
